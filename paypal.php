9759561ccf56b65500cc04529dff967769d75e6e
Michael Dekker <git@michaeldekker.nl>
2017-05-08 06:49:25 +0200

    bump to v5.0.1

--- paypal.php
+++ paypal.php
@@ -121,7 +121,7 @@ class PayPal extends \PaymentModule
     {
         $this->name = 'paypal';
         $this->tab = 'payments_gateways';
-        $this->version = '5.0.0';
+        $this->version = '5.0.1';
         $this->author = 'thirty bees';
 
         $this->currencies = true;
