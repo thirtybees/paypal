3a83812fd1a9a1235282e2d9ada061d10cdb419b
Petr Hučík <petr@getdatakick.com>
2019-01-24 09:44:22 +0100

    Incremented build

--- paypal.php
+++ paypal.php
@@ -123,7 +123,7 @@ class PayPal extends \PaymentModule
     {
         $this->name = 'paypal';
         $this->tab = 'payments_gateways';
-        $this->version = '5.4.1';
+        $this->version = '5.4.2';
         $this->author = 'thirty bees';
 
         $this->currencies = true;
