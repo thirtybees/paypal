781f3ad363df3d1c4968c32d4a73cf9b9bca25d7
Yaniv Mirel <yanivmirel@gmail.com>
2020-09-13 08:46:03 +0300

    customer is not in context, so just grab id from $customer.
    Mentioned here: https://forum.thirtybees.com/topic/3759-paypal-new-user-failed-to-pay/#comment-37483
    Thanks to Tuan Pham

--- controllers/front/incontextvalidate.php
+++ controllers/front/incontextvalidate.php
@@ -72,7 +72,7 @@ class paypalincontextvalidateModuleFrontController extends ModuleFrontController
                 $customer->add();
 
                 $ppc = new PayPalCustomer();
-                $ppc->id_customer = $this->context->customer->id;
+                $ppc->id_customer = $customer->id;
                 $ppc->paypal_email = $email;
                 $ppc->add();
             }
