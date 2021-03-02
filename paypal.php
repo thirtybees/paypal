8b0813f0e24175b6f5ffd7c72f4816f0a4217032
Petr Hučík <petr@getdatakick.com>
2021-03-02 15:26:45 +0100

    Release version 5.4.4

--- paypal.php
+++ paypal.php
@@ -123,7 +123,7 @@ class PayPal extends \PaymentModule
     {
         $this->name = 'paypal';
         $this->tab = 'payments_gateways';
-        $this->version = '5.4.3';
+        $this->version = '5.4.4';
         $this->author = 'thirty bees';
 
         $this->currencies = true;
