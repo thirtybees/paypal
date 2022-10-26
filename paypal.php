de6447f4fc496ccac671bc8e269cddf535e74414
Petr Hučík <petr@getdatakick.com>
2022-10-26 10:55:53 +0200

    Release paypal v5.6.0

--- paypal.php
+++ paypal.php
@@ -123,7 +123,7 @@ class PayPal extends PaymentModule
     {
         $this->name = 'paypal';
         $this->tab = 'payments_gateways';
-        $this->version = '5.5.1';
+        $this->version = '5.6.0';
         $this->author = 'thirty bees';
 
         $this->currencies = true;
